00f8beafa776dc59ade58382223adcf4bb636187
lyubovkupriyanova <lkupriyanova@ozon.ru>
2020-09-13 02:28:30 +0300

    tg upd

--- hackabot/telegram.py
+++ hackabot/telegram.py
@@ -9,11 +9,33 @@ from typing import Any, DefaultDict
 import requests
 import telebot
 import granula
+import time
+import json
 
 from speech_pack.tinkoff_stt import file_to_text
 from speech_pack.tinkoff_tts import generate
 
+from .link_stt import parse
+
 logger = logging.getLogger('telegram')
+
+
+def training_parse(message: telebot.types.Message, state: int, req: str):
+    req = ''.join(x for x in req if x.isalpha() or x == ' ')
+    req = req.lower().split()
+
+    if state == 0:
+        for r in req:
+            if r.startswith('кофе'):
+                #MAKE VOICE
+                _send(message, response='Есть классный Мираторг в ТЦ Амальтея в 7 минут пешком от тебя.\
+                     Получи кешбек 10% и разблокируй новую локацию! Адрес кину в сообщении. Скажи готово, когда оплатишь и насладишься кофе')
+                #END VOICE
+                _send(message, response='Мираторг, Большой бульвар, 40')
+    elif state == 1:
+        for r in req:
+            if r.startswith('рейт'):
+                _send(message, response='Ваш рейтинг - N по Москве. Вы в топ-20%! Войдите в топ-10% и удвойте свой кешбек!')
 
 
 def get_full_name(user: telebot.types.User) -> str:
@@ -41,7 +63,69 @@ def run_bot(token: str):
     @bot.message_handler(commands=['start'])
     def _start(message: telebot.types.Message):
         with locks[message.chat.id]:
-            _send(message, response='Задавайте ваши вопросы')
+            _send(message, response='Открывай новые локации, \
+             лови кешбек, поднимайся в рейтинге. Трать с умом и получай еще больше!')
+            time.sleep(1)
+            _send(message, response='Сейчас расскажу правила и проведу через короткое обучение. \
+                 В Игру, наш финансово грамотный друг, в игру!')
+            time.sleep(1)
+            _send(message, response='Суть проста: набираешь как можно больше кэшбэка \
+                 за время акции (я помогу тебе в этом!) - получаешь повышенный кешбэк НА ВСЕ в течение 3х месяцев. И это еще не все! Получай скидки от партнеров за ачивки в игре.')
+            time.sleep(1)
+            _send(message, response='Что за ачивки? Получай их за открытие новых локаций! \
+                Пока открыто очень мало, смотри:')
+            #отправить картонку до
+            _send(message, response='A теперь спроси меня, где кофейня с лучшим кешбэком рядом с тобой? Можно и нужно голосом!')
+        try:
+            with open('users.json', 'r') as f:
+                tmp = json.load(f)
+        except:
+            tmp = {}
+        tmp[message.chat.id] = {'current_state': 0}
+        with open('users.json', 'w') as f:
+            json.dump(tmp, f)
+        
+
+    @bot.message_handler(content_types=['location'])
+    def handle_location(message):
+        sq_size = 25 / 10000
+        latitude, longitude = message.location.latitude, message.location.longitude
+        coor0 = (latitude + sq_size / 2, longitude + sq_size / 2)
+        coor1 = (latitude - sq_size / 2, longitude - sq_size / 2)
+
+        headers = {'Content-Type': 'application/json'}
+        params = {
+        "geo_query": {
+            "bottom_right": {
+                "lat": 55.73741399385868,
+                "lon": 37.56961595778349
+            },
+            "top_left": {
+                "lat": 55.742244061297384,
+                "lon": 37.56546389822844
+            }
+        },
+        "query": places_query,
+        "count": 3
+        }
+
+        r = requests.post('https://api-common-gw.tinkoff.ru/search/api/v1/search_merchants',
+        headers = headers, json = params)
+
+        search_merchs = r.json()['search_result']['hits']
+        #global geo, names, adresses
+        #geo = search_merchs['geo']
+        names = [merch['merchant_display_name'] for merch in search_merchs]
+        adresses = [merch['geo'][0]['address'] for merch in search_merchs]
+        name_address = [x + ' ' + y for x,y in zip(names, adresses)]
+
+        _send(message, response='Нашел лучшие места. Сейчас кину в сообщении') #replace witj speech
+        _send(message, response=name_address[0])
+        #add location
+        _send(message, response=name_address[1])
+        #add location
+        _send(message, response=name_address[2])
+        #add location
 
     def _get_echo_response(text: str, user_id: str) -> str:
         return f'Добрый день, спутник! Не хотите немного кэшбэка?\n' \
@@ -82,7 +166,14 @@ def run_bot(token: str):
                 f.write(voice_message)
 
                 response = file_to_text(voice_file_name)
-                bot.send_message(message.chat.id, response)
+                #bot.send_message(message.chat.id, response)
+                with open('users.json', 'r') as f:
+                    tmp = json.load(f)
+                    if tmp[message.chat.id]['current_state'] == 0 or tmp[message.chat.id]['current_state'] == 1:
+                        training_parse(message, tmp[message.chat.id]['current_state'], response)
+                    else:
+                        parse(user_id=message.chat.id)
+
         except Exception as e:
             bot.send_message(message.chat.id, 'Не удалось распознать текст (((. У меня ляпки')
 
